2c3e7f97bea9791fcebadcd863c60e0bcb96a3dc
Paulo Casa <paulo_sa_araujo@hotmail.com>
2024-09-26 23:41:39 -0300

    Adding TO DOs and scratching functions

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
 import streamlit as st
 from transformers import pipeline
+from dotenv import load_dotenv
 import pandas as pd
 
-class ChatBot:
-    def __init__(self, name, description, conversation):
-        self.name = name
-        self.description = description
-        self.conversation = conversation
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain_community.document_loaders import CSVLoader
+
+
+def main():
+
+    # Load environment variables
+    load_dotenv()
+
+    # Setup
+    databases_path = st.secrets["databases_path"]
+
+    @st.cache_resource
+    def load_model():
+        '''
+        Load the Language model
+        '''
+        return pipeline("question-answering", model="distilbert-base-uncased", tokenizer="distilbert-base-uncased")
+    
+    # Carregar a base de dados Excel
+    @st.cache_data
+    def load_databases(file_path):
+
+        # TODO: IMPLEMENTAR LOADER PARA LER O BIZUARIO EM WORD E RESPONDER A UM QUESTIONAMENTO, TESTE. USAR LLM GRATUITA HUGGING FACE
+        try:
+            df = pd.read_excel(file_path)
+            return df
+        except Exception as e:
+            st.error(f"Erro ao carregar o arquivo Excel: {e}")
+            return None
+
+if __name__ == "__main__":
+    main()

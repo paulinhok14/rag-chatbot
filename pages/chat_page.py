386c4eb7bdbcb1c4db764c5b02307d5bd074e239
Paulo Casa <paulo_sa_araujo@hotmail.com>
2024-10-14 22:38:55 -0300

    Adjusting Chain to have all steps connected

--- pages/chat_page.py
+++ pages/chat_page.py
@@ -8,12 +8,23 @@ from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import DocArrayInMemorySearch # Get a better solution instead of saving in memory. At first, works.
 from langchain.prompts import PromptTemplate
+from operator import itemgetter
 
 # Model instancing
 MODEL = 'llama3.2'
 # MODEL = 'llama2:7b-chat'
 model = Ollama(model=MODEL)
 embeddings = OllamaEmbeddings(model=MODEL)
+
+# Knowledge Base
+bizuario_doc_path = os.path.join(os.getcwd(), r'src\databases\Bizuario Geral.docx')
+# Reading file as documents
+loader = Docx2txtLoader(bizuario_doc_path)
+bizuario_doc = loader.load_and_split()
+# Starting VectorStore
+vectorstore = DocArrayInMemorySearch.from_documents(bizuario_doc, embedding=embeddings)
+# Retriever
+retriever = vectorstore.as_retriever()
 
 # Initialize Page state
 description = 'Materials Solution AI Assistant'
@@ -22,14 +33,6 @@ knowledge_base_path = os.path.join(os.path.dirname(__file__), 'association_rules
 # Page components
 st.write(description)
 st.subheader('Ask me anything...')
-
-# Knowledge Base
-bizuario_doc_path = os.path.join(os.getcwd(), r'src\databases\Bizuario Geral.docx')
-# Reading file as documents
-loader = Docx2txtLoader(bizuario_doc_path)
-bizuario_doc = loader.load_and_split()
-# Starting VectorStore
-# vectorstore = DocArrayInMemorySearch.from_documents(bizuario_doc, embedding=embeddings)
 
 def stream_text(text):
     '''
@@ -40,9 +43,6 @@ def stream_text(text):
         time.sleep(0.04)
 
 
-def generate_response(question):
-      pass
-
 # Prompt (System Message. Define Tasks, Tone, Behaviour and Safety params)
 template = '''
 Você é um assistente virtual de uma área de materiais chamada Spare Parts Planning.
@@ -50,6 +50,7 @@ Sua função será responder à questões genéricas feitas pelos colaboradores 
 Vou lhe passar um documento com diversas informações relevantes, tais como significado de siglas, telefones úteis, explicação de processos, etc, para que você use como referência para responder ao questionamento do usuário.
 
 Siga todas as regras abaixo:
+
 1/ Você deve buscar se comportar de maneira sempre cordial e solícita para atender aos questionamentos dos usuários.
 
 2/ Algumas linhas do documento fornecido podem conter informações irrelevantes. Preste atenção ao conteúdo útil da mensagem.
@@ -59,32 +60,47 @@ A única informação que pode ser passada relacionada aos colaboradores é o re
 
 4/ Em hipótese alguma envolva-se em discussões de cunho pessoal, sobre tecer opiniões sobre um colaborador ou outro. Caso a pergunta seja neste sentido, recuse-se gentilmente a opinar e ofereça ao usuário ajuda nas questões relevantes.
 
-Aqui está uma pergunta recebida de um usuário.
+Aqui está uma pergunta recebida de um usuário:
 {question}
 
 Aqui está o documento com as informações relevantes mencionadas.
-Esse arquivo servirá de base para que você compreenda nosso contexto de negócio, organização e nossas siglas mais comumente utilizadas.
+Esse arquivo servirá de base para que você compreenda nosso contexto de negócio, organização e nossas siglas mais comumente utilizadas:
 {context}
 
 Escreva a melhor resposta que atende ao questionamento do usuário:
 '''
 
 prompt = PromptTemplate.from_template(template)
-# print(prompt.format(question=question, context=bizuario_doc))
+
+
+def generate_response(question):
+      # Creating and invoking Chain    
+      chain = (
+           {
+                'context': itemgetter('question') | retriever, # the context will come from a retriever (relevant docs), given a question
+                'question': itemgetter('question')
+           }
+           | prompt
+           | model
+      )
+
+      answer = chain.invoke({'question': question})
+      return answer
+
 
 # Question
-message = st.text_area(label='message', label_visibility='hidden', placeholder='Ex: "O que significa a sigla EPEP?"', height=150)
+question = st.text_area(label='message', label_visibility='hidden', placeholder='Ex: "O que significa a sigla EPEP?"', height=150)
 
 # Chat components
-if message:
+if question:
       answer_space = st.empty()
-      while True: # Create condition in which the response is already generated so the loop should stop
-        answer_space.write_stream(stream_text("Investigating for answers. Please wait... :male-detective:"))
-        time.sleep(2)
+    #   while True: # Create condition in which the response is already generated so the loop should stop
+    #     answer_space.write_stream(stream_text("Investigating for answers. Please wait... :male-detective:"))
+    #     time.sleep(2)
 
-      #result = generate_response(question)
+      result = generate_response(question)
 
-      #st.info(result)
+      st.info(result)
 
     
 
